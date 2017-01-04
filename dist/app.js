06869caca6b65cd7d4ddb399f3c5e5283577f348
jeffswitzer <jeff.switzer@gmail.com>
2017-01-04 12:34:12 -0700

    710 Make year text dynamic on phenology maps landing page

--- dist/app.js
+++ dist/app.js
@@ -50089,6 +50089,13 @@
 	            this.selectedDate = now.toISOString().slice(0, 10);
 	            this.yearlyTimeStep = false;
 	        }
+	        if (layer.name.includes("ncep_historic")) {
+	            this.selectedDoy = null;
+	            this.selectedDate = "2016-01-01";
+	            this.selectedYear = 2016;
+	            this.yearlyTimeStep = true;
+	            this.setYears(2016, this.previousYear);
+	        }
 	        this.selectedLayer = layer;
 	    };
 	    AppComponent.prototype.setYears = function (minYear, maxYear) {
@@ -50126,22 +50133,27 @@
 	        var res = rightNow.toISOString().slice(0, 10);
 	        if (!this.selectedLayer)
 	            return "Select a layer to view its dimension range.";
+	        if (this.selectedLayer.name.includes("ncep_historic")) {
+	            this.minDate = "2016-01-01";
+	            this.maxDate = (rightNow.getFullYear() - 1).toString() + "-01-01";
+	            return "2016-01-01 Through " + (rightNow.getFullYear() - 1).toString() + "-01-01";
+	        }
 	        if (this.selectedLayer.dimensionRange === "1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0,16.0,17.0,18.0,19.0,20.0,21.0,22.0,23.0,24.0,25.0,26.0,27.0,28.0,29.0,30.0,31.0,32.0,33.0,34.0,35.0,36.0,37.0,38.0,39.0,40.0,41.0,42.0,43.0,44.0,45.0,46.0,47.0,48.0,49.0,50.0,51.0,52.0,53.0,54.0,55.0,56.0,57.0,58.0,59.0,60.0,61.0,62.0,63.0,64.0,65.0,66.0,67.0,68.0,69.0,70.0,71.0,72.0,73.0,74.0,75.0,76.0,77.0,78.0,79.0,80.0,81.0,82.0,83.0,84.0,85.0,86.0,87.0,88.0,89.0,90.0,91.0,92.0,93.0,94.0,95.0,96.0,97.0,98.0,99.0,100.0,101.0,102.0,103.0,104.0,105.0,106.0,107.0,108.0,109.0,110.0,111.0,112.0,113.0,114.0,115.0,116.0,117.0,118.0,119.0,120.0,121.0,122.0,123.0,124.0,125.0,126.0,127.0,128.0,129.0,130.0,131.0,132.0,133.0,134.0,135.0,136.0,137.0,138.0,139.0,140.0,141.0,142.0,143.0,144.0,145.0,146.0,147.0,148.0,149.0,150.0,151.0,152.0,153.0,154.0,155.0,156.0,157.0,158.0,159.0,160.0,161.0,162.0,163.0,164.0,165.0,166.0,167.0,168.0,169.0,170.0,171.0,172.0,173.0,174.0,175.0,176.0,177.0,178.0,179.0,180.0,181.0,182.0,183.0,184.0,185.0,186.0,187.0,188.0,189.0,190.0,191.0,192.0,193.0,194.0,195.0,196.0,197.0,198.0,199.0,200.0,201.0,202.0,203.0,204.0,205.0,206.0,207.0,208.0,209.0,210.0,211.0,212.0,213.0,214.0,215.0,216.0,217.0,218.0,219.0,220.0,221.0,222.0,223.0,224.0,225.0,226.0,227.0,228.0,229.0,230.0,231.0,232.0,233.0,234.0,235.0,236.0,237.0,238.0,239.0,240.0,241.0,242.0,243.0,244.0,245.0,246.0,247.0,248.0,249.0,250.0,251.0,252.0,253.0,254.0,255.0,256.0,257.0,258.0,259.0,260.0,261.0,262.0,263.0,264.0,265.0,266.0,267.0,268.0,269.0,270.0,271.0,272.0,273.0,274.0,275.0,276.0,277.0,278.0,279.0,280.0,281.0,282.0,283.0,284.0,285.0,286.0,287.0,288.0,289.0,290.0,291.0,292.0,293.0,294.0,295.0,296.0,297.0,298.0,299.0,300.0,301.0,302.0,303.0,304.0,305.0,306.0,307.0,308.0,309.0,310.0,311.0,312.0,313.0,314.0,315.0,316.0,317.0,318.0,319.0,320.0,321.0,322.0,323.0,324.0,325.0,326.0,327.0,328.0,329.0,330.0,331.0,332.0,333.0,334.0,335.0,336.0,337.0,338.0,339.0,340.0,341.0,342.0,343.0,344.0,345.0,346.0,347.0,348.0,349.0,350.0,351.0,352.0,353.0,354.0,355.0,356.0,357.0,358.0,359.0,360.0,361.0,362.0,363.0,364.0,365.0")
 	            return "1-365";
 	        if (this.selectedLayer.dimensionRange.includes("1981")) {
 	            this.minDate = "1981-01-01";
 	            this.maxDate = (rightNow.getFullYear() - 1).toString() + "-01-01";
-	            return "1981-01-01 through " + (rightNow.getFullYear() - 1).toString() + "-01-01";
+	            return "1981-01-01 Through " + (rightNow.getFullYear() - 1).toString() + "-01-01";
 	        }
 	        if (this.selectedLayer.dimensionRange.includes("1880")) {
 	            this.minDate = "1880-01-01";
 	            this.maxDate = "2013-01-01";
-	            return "1880-01-01 through 2013-01-01";
+	            return "1880-01-01 Through 2013-01-01";
 	        }
 	        if (this.selectedLayer.dimensionRange.includes("2016-01-01")) {
 	            this.minDate = "2016-01-01";
 	            this.maxDate = res;
-	            return "2016-01-01 through " + res;
+	            return "2016-01-01 Through " + res;
 	        }
 	    };
 	    AppComponent.prototype.getLayerStyle = function (layer) {
@@ -50181,6 +50193,105 @@
 	        else
 	            return "";
 	    };
+	    AppComponent.prototype.getCaption = function (layer) {
+	        var dateText = "";
+	        var yearText = "";
+	        var doyText = "";
+	        if (this.selectedLayerHasDate() && this.selectedDate)
+	            dateText = this.selectedDate;
+	        if (this.selectedLayerHasYear() && this.selectedYear)
+	            yearText = this.selectedYear.toString();
+	        if (this.selectedLayerHasDoy() && this.selectedDoy)
+	            doyText = this.selectedDoy.toString();
+	        // climate captions
+	        if (layer.name.includes("tmin"))
+	            return "Minimum Temperatures in Celsius on " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("tmax"))
+	            return "Maximum Temperatures in Celsius on " + dateText + " Using NCEP Data";
+	        // agdd captions
+	        if (layer.name.includes("gdd:agdd_anomaly_50f"))
+	            return "Accumulated Growing Degree Days Anomaly 50F Base Temp Through " + dateText + " Using NCEP and PRISM Data";
+	        if (layer.name.includes("gdd:agdd_anomaly"))
+	            return "Accumulated Growing Degree Days Anomaly 32F Base Temp Through " + dateText + " Using NCEP and PRISM Data";
+	        if (layer.name.includes("gdd:30yr_avg_agdd_50f"))
+	            return "30-Year Average Temperature Accumulations 50F Base Temp Through Day of Year " + doyText + " Using PRISM Data";
+	        if (layer.name.includes("gdd:30yr_avg_agdd"))
+	            return "30-Year Average Temperature Accumulations 32F Base Temp Through Day of Year " + doyText + " Using PRISM Data";
+	        if (layer.name.includes("gdd:agdd_50f"))
+	            return "Accumulated Growing Degree Days 50F Base Temp Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("gdd:agdd_prism"))
+	            return "PRISM Accumulated Growing Degree Days 32F Base Temp Through " + dateText + " Using PRISM Data";
+	        if (layer.name.includes("gdd:agdd"))
+	            return "Accumulated Growing Degree Days 32F Base Temp Through " + dateText + " Using NCEP Data";
+	        // spring index historic ncep
+	        if (layer.name.includes("arnoldred_leaf_ncep_historic"))
+	            return "Arnold Red Honeysuckle First Leaf " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("zabelli_leaf_ncep_historic"))
+	            return "Zabelli First Leaf " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("lilac_leaf_ncep_historic"))
+	            return "Lilac First Leaf " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("arnoldred_bloom_ncep_historic"))
+	            return "Arnold Red Honeysuckle First Bloom " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("zabelli_bloom_ncep_historic"))
+	            return "Zabelli First Bloom " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("lilac_bloom_ncep_historic"))
+	            return "Lilac First Bloom " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("average_leaf_ncep_historic"))
+	            return "Spring Index First Leaf " + yearText + " Using NCEP Data";
+	        if (layer.name.includes("average_bloom_ncep_historic"))
+	            return "Spring Index First Bloom " + yearText + " Using NCEP Data";
+	        // spring index current year
+	        if (layer.name.includes("si-x:arnoldred_leaf_ncep"))
+	            return "Arnold Red Honeysuckle First Leaf Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:zabelli_leaf_ncep"))
+	            return "Zabelli First Leaf Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:lilac_leaf_ncep"))
+	            return "Lilac First Leaf Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:arnoldred_bloom_ncep"))
+	            return "Arnold Red Honeysuckle First Bloom Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:zabelli_bloom_ncep"))
+	            return "Zabelli First Bloom Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:lilac_bloom_ncep"))
+	            return "Lilac First Bloom Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:average_leaf_ncep"))
+	            return "Spring Index First Leaf Through " + dateText + " Using NCEP Data";
+	        if (layer.name.includes("si-x:average_bloom_ncep"))
+	            return "Spring Index First Bloom Through " + dateText + " Using NCEP Data";
+	        // spring index historic prism
+	        if (layer.name.includes("si-x:arnoldred_leaf_prism"))
+	            return "Arnold Red Honeysuckle First Leaf " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:zabelli_leaf_prism"))
+	            return "Zabelli First Leaf " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:lilac_leaf_prism"))
+	            return "Lilac First Leaf " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:arnoldred_bloom_prism"))
+	            return "Arnold Red Honeysuckle First Bloom " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:zabelli_bloom_prism"))
+	            return "Zabelli First Bloom " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:lilac_bloom_prism"))
+	            return "Lilac First Bloom " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:average_leaf_prism"))
+	            return "Spring Index First Leaf " + yearText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:average_bloom_prism"))
+	            return "Spring Index First Bloom " + yearText + " Using PRISM Data";
+	        // 30 year average si-x
+	        if (layer.name.includes("si-x:30yr_avg_six_leaf"))
+	            return "30-Year Average Spring Index First Leaf Through Day of Year " + doyText + " Using PRISM Data";
+	        if (layer.name.includes("si-x:30yr_avg_six_bloom"))
+	            return "30-Year Average Spring Index First Bloom Through Day of Year " + doyText + " Using PRISM Data";
+	        // spring index anomaly
+	        if (layer.name.includes("si-x:leaf_anomaly"))
+	            return "Spring Index First Leaf Anomaly Through " + dateText + " Using NCEP and PRISM Data";
+	        if (layer.name.includes("si-x:bloom_anomaly"))
+	            return "Spring Index First Bloom Anomaly Through " + dateText + " Using NCEP and PRISM Data";
+	        // spring index BEST
+	        if (layer.name.includes("leaf_best"))
+	            return "Spring Index First Leaf " + yearText + " Using BEST Data";
+	        if (layer.name.includes("bloom_best"))
+	            return "Spring Index First Bloom " + yearText + " Using BEST Data";
+	        else
+	            return "";
+	    };
 	    AppComponent.prototype.getGeoserverUrl = function () {
 	        var url = "";
 	        if (location.hostname.includes("local"))
@@ -50205,6 +50316,8 @@
 	                    url = url.concat("&format_options=layout:provisional");
 	                else
 	                    url = url.concat("&format_options=layout:provisional_no_ramp");
+	                if (this.getCaption(this.selectedLayer) !== "")
+	                    url = url.concat("&env=the_caption:" + this.getCaption(this.selectedLayer));
 	            }
 	            if (this.urlWidth)
 	                url = url.concat("&width=" + this.urlWidth.toString());
