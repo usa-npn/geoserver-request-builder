1f8dd5323ea6aefb1d2f01e33f7c0abe7f81e98d
jeffswitzer <jeff.switzer@gmail.com>
2017-01-04 16:17:25 -0700

    extension tif should be tiff

--- app/geoserver.service.ts
+++ app/geoserver.service.ts
@@ -112,7 +112,7 @@ export class GeoserverService {
         // {name: "GIF", syntax: "gif", description: ""},
         // {name: "PNG", syntax: "png", description: "Portable Network Graphics is a raster graphics file format that supports lossless data compression."},
         {name: "GeoTiff", syntax: "geotiff", description: ""},
-        {name: "Tiff", syntax: "tif", description: ""},
+        {name: "Tiff", syntax: "tiff", description: ""},
         // {name: "BMP", syntax: "bmp", description: ""},
         // {name: "GTopo30", syntax: "application/gtopo30", description: ""},
         {name: "ArcGrid", syntax: "ArcGrid", description: ""},
